dbfefe3ed214396bda9040a078a3e1e0aef43531
Lhakpa Lama <lhakpa1712@gmail.com>
2021-06-06 13:27:43 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -22,7 +22,7 @@ class CommentForm extends Component{
     }
     handleSubmit(values){
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author,values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author,values.text);
     }
     toggleModal(){ 
         this.setState({
@@ -108,7 +108,7 @@ class CommentForm extends Component{
         )
     }
 }
-function RenderComments({comments, addComment, campsiteId}){
+function RenderComments({comments, postComment, campsiteId}){
         if(comments){
             return(
                 <div className="col-md-5 m-1">
@@ -124,7 +124,7 @@ function RenderComments({comments, addComment, campsiteId}){
                         }
                    
                     )}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+                 <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
             );
             } 
@@ -182,7 +182,7 @@ function CampsiteInfo(props){
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                             comments={props.comments}
-                            addComment={props.addComment}
+                            postComment={props.postComment}
                             campsiteId={props.campsite.id} />
                     </div>
                 </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,14 +8,14 @@ import {connect} from 'react-redux';
 import Contact from './ContactComponent'
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
-import {addComment,fetchCampsites,fetchComments,fetchPromotions} from '../redux/ActionCreators';
+import {postComment,fetchCampsites,fetchComments,fetchPromotions} from '../redux/ActionCreators';
 import { actions} from 'react-redux-form';
 
 
 const mapDispatchToProps ={
-  addComment:(campsiteId,rating,author,text)=>(addComment(campsiteId,rating,author,text)),
+  postComment:(campsiteId,rating,author,text)=>(postComment(campsiteId,rating,author,text)),
   fetchCampsites:()=> (fetchCampsites()),
-  resetFeedbackForm:() =>(actions.reset('feedbackForm')),
+  resetFeedbackForm:() => (actions.reset('feedbackForm')),
   fetchComments:()=> (fetchComments()),
   fetchPromotions:()=>(fetchPromotions())
 };
@@ -57,7 +57,7 @@ class Main extends Component {
                     errMess={this.props.campsites.errMess}
                     comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
                     commentsErrMess={this.props.comments.errMess}
-                    addComment={this.props.addComment}
+                    postComment={this.props.postComment}
                 />
         )
       }
